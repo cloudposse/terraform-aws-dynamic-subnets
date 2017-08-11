27bfe60c10dbd56f8d6076e666b5325ce3bb5989
comeanother <const.bon@gmail.com>
2017-08-11 14:51:23 +0300

    Added precise `tags` reference in `tf_label` module. Fixed issue when `vpc_default_route_table` already exists

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 module "tf_label" {
-  source    = "git::https://github.com/cloudposse/tf_label.git?ref=0.1.0"
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
   namespace = "${var.namespace}"
   stage     = "${var.stage}"
   name      = "${var.name}"
--- public.tf
+++ public.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_route_table" "public" {
-  count  = "${1 - signum(length(var.vpc_default_route_table))}"
+  count  = "${signum(length(var.vpc_default_route_table)) == 1 ? 0 : 1}"
   vpc_id = "${data.aws_vpc.default.id}"
 
   route {
@@ -19,10 +19,14 @@ resource "aws_route_table" "public" {
   tags = "${module.tf_label.tags}"
 }
 
-
-
-resource "aws_route_table_association" "public" {
-  count = "${length(var.availability_zones)}"
+resource "aws_route_table_association" "public_new" {
+  count = "${signum(length(var.vpc_default_route_table)) == 1 ? 0 : length(var.availability_zones)}"
   subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public_exists" {
+  count = "${signum(length(var.vpc_default_route_table)) == 1 ? length(var.availability_zones) : 0}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${var.vpc_default_route_table}"
 }
