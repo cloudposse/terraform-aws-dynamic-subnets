0a59eb0882aaaf070b815b4c63628dbb316aca09
comeanother <const.bon@gmail.com>
2017-08-15 18:36:35 +0300

    Added  and  suffix in  for subnets accordingly

--- main.tf
+++ main.tf
@@ -6,13 +6,6 @@ provider "aws" {
   region = "${var.region}"
 }
 
-module "tf_label" {
-  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.name}"
-}
-
 # Get object aws_vpc by vpc_id
 data "aws_vpc" "default" {
   id = "${var.vpc_id}"
--- private.tf
+++ private.tf
@@ -1,10 +1,17 @@
+module "private_label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "${var.name}-private"
+}
+
 resource "aws_subnet" "private" {
   count = "${length(var.availability_zones)}"
 
   vpc_id            = "${data.aws_vpc.default.id}"
   availability_zone = "${element(var.availability_zones, count.index)}"
   cidr_block        = "${cidrsubnet(data.aws_vpc.default.cidr_block, length(var.availability_zones), length(var.availability_zones)  + count.index)}"
-  tags              = "${module.tf_label.tags}"
+  tags              = "${module.private_label.tags}"
 }
 
 resource "aws_route_table" "private" {
@@ -16,7 +23,7 @@ resource "aws_route_table" "private" {
     nat_gateway_id = "${element(aws_nat_gateway.default.*.id, count.index)}"
   }
 
-  tags = "${module.tf_label.tags}"
+  tags = "${module.private_label.tags}"
 }
 
 resource "aws_route_table_association" "private" {
