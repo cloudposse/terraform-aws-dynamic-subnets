0111a0ecff822a1964e7a713a8736680c2ac9f92
aknysh <aknysh>
2017-08-16 13:46:14 -0400

    Added `_ids` suffix to `outputs.tf` since the outputs are subnet IDs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "public_subnets" {
+output "public_subnet_ids" {
   value = ["${aws_subnet.public.*.id}"]
 }
 
-output "private_subnets" {
+output "private_subnet_ids" {
   value = ["${aws_subnet.private.*.id}"]
 }
