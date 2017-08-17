4611bb2220a8f15a3f0e70eb77d5d6cf002c060e
Andriy Knysh <aknysh@users.noreply.github.com>
2017-08-16 21:41:14 -0400

    Add Outputs for public & private subnet ids (#5)
    * Added outputs.tf
    
    * Added `_ids` suffix to `outputs.tf` since the outputs are subnet IDs

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "aws_vpc" "default" {
   id = "${var.vpc_id}"
 }
 
-# Get all subnets from the necessary vpc
+# Get all subnets from the VPC
 data "aws_subnet_ids" "all" {
   vpc_id = "${data.aws_vpc.default.id}"
 }
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "public_subnet_ids" {
+  value = ["${aws_subnet.public.*.id}"]
+}
+
+output "private_subnet_ids" {
+  value = ["${aws_subnet.private.*.id}"]
+}
