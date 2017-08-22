5e68e1b192fde7aaa2fc23799e89a6f33ec3c7c5
aknysh <aknysh>
2017-08-21 22:46:28 -0400

    Place NAT Gateways into public subnets

--- gws.tf
+++ gws.tf
@@ -10,7 +10,7 @@ resource "aws_eip" "default" {
 resource "aws_nat_gateway" "default" {
   count         = "${length(var.availability_zones)}"
   allocation_id = "${element(aws_eip.default.*.id, count.index)}"
-  subnet_id     = "${element(aws_subnet.private.*.id, count.index)}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
 
   lifecycle {
     create_before_destroy = true
