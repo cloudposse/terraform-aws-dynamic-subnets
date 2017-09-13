29af0af30e4877f1367ac3e66910e7c4d66c282e
Sergey Vasilyev <s2504s@gmail.com>
2017-09-13 21:15:14 +0300

    Fix cidr_block

--- public.tf
+++ public.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public" {
   count             = "${length(var.availability_zones)}"
   vpc_id            = "${data.aws_vpc.default.id}"
   availability_zone = "${element(var.availability_zones, count.index)}"
-  cidr_block        = "${cidrsubnet(signum(length(var.cidr_block)) == 1 ? var.cidr_block : data.aws_vpc.default.cidr_block, ceil(log(length(data.aws_availability_zones.available.names) * 2, 2)), count.index)}"
+  cidr_block        = "${cidrsubnet(signum(length(var.cidr_block)) == 1 ? var.cidr_block : data.aws_vpc.default.cidr_block, ceil(log(length(data.aws_availability_zones.available.names) * 2, 2)), length(data.aws_availability_zones.available.names) + count.index)}"
   tags              = "${module.public_label.tags}"
 }
 
