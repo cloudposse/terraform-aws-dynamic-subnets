114d40287f69eafbabaffbf6a33b57f960ada5ee
Sergey Vasilyev <s2504s@gmail.com>
2017-09-14 15:52:08 +0300

    Added public rt_ids to output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "private_subnet_ids" {
   value = ["${aws_subnet.private.*.id}"]
 }
 
+output "public_route_table_ids" {
+  value = ["${aws_route_table.public.*.id}"]
+}
+
 output "private_route_table_ids" {
   value = ["${aws_route_table.private.*.id}"]
 }
