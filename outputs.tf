69df4e2b543b6281f819e5a19601f333f00f8041
Sergey Vasilyev <s2504s@gmail.com>
2017-09-14 16:23:57 +0300

    Add route table IDs outputs (#16)

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = ["${aws_subnet.private.*.id}"]
 }
+
+output "public_route_table_ids" {
+  value = ["${aws_route_table.public.*.id}"]
+}
+
+output "private_route_table_ids" {
+  value = ["${aws_route_table.private.*.id}"]
+}
