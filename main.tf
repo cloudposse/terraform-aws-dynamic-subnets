65c7e396df96d0f87040c8637491f73d93926e6d
aknysh <aknysh>
2018-01-12 09:40:15 -0500

    Remove `provider "aws"`

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 0.10.2"
 }
 
-provider "aws" {
-  region = "${var.region}"
-}
-
 # Get object aws_vpc by vpc_id
 data "aws_vpc" "default" {
   id = "${var.vpc_id}"
