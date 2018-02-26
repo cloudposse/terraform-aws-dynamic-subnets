f687b265aa9b7b142fe7a9e885b7dbc45a85b785
David Cowan <dcowan@vestmark.com>
2018-02-26 13:24:30 -0600

    Revert "Updating to use supplied availablity zones for cidr calculation"
    This reverts commit ad782e89c5597ed8bfe8fb4e0e2df0de9754a1e8.

--- public.tf
+++ public.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "public" {
   count             = "${length(var.availability_zones)}"
   vpc_id            = "${data.aws_vpc.default.id}"
   availability_zone = "${element(var.availability_zones, count.index)}"
-  cidr_block        = "${cidrsubnet(signum(length(var.cidr_block)) == 1 ? var.cidr_block : data.aws_vpc.default.cidr_block, ceil(log(length(var.availability_zones) * 2, 2)), length(var.availability_zones) + count.index)}"
+  cidr_block        = "${cidrsubnet(signum(length(var.cidr_block)) == 1 ? var.cidr_block : data.aws_vpc.default.cidr_block, ceil(log(length(data.aws_availability_zones.available.names) * 2, 2)), length(data.aws_availability_zones.available.names) + count.index)}"
 
   tags = {
     "Name"      = "${module.public_subnet_label.id}${var.delimiter}${replace(element(var.availability_zones, count.index),"-",var.delimiter)}"
