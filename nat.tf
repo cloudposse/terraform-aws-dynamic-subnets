2519274d72e8e96655762ff728905026c01f00eb
David Cowan <dcowan@vestmark.com>
2018-05-07 13:52:29 -0500

    Cleanup from pull request review

--- nat.tf
+++ nat.tf
@@ -1,10 +1,10 @@
 module "nat_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  delimiter  = "${var.delimiter}"
-  tags       = "${var.tags}"
+  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "${var.name}"
+  delimiter = "${var.delimiter}"
+  tags      = "${var.tags}"
 }
 
 locals {
