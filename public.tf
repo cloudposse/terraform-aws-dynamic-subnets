c1593ff9d7ce33a60b92bb1ba3510b6f7ab4dd73
David Cowan <dcowan@vestmark.com>
2018-05-14 09:34:14 -0500

    Adding attributes to label and description to max_subnets variable

--- public.tf
+++ public.tf
@@ -1,8 +1,8 @@
 module "public_subnet_label" {
-  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.name}"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
   attributes = ["public"]
 }
 
