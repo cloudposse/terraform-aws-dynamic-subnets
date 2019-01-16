ca34b38c7100d4cfd7cd7259fa31568fe6cee06e
Steven Nemetz <steven@appzen.com>
2019-01-16 09:19:08 -0800

    Fix private subnet name

--- private.tf
+++ private.tf
@@ -15,7 +15,7 @@ module "private_subnet_label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
-  name       = "subnet"
+  name       = "${var.name}"
   attributes = ["private"]
   tags       = "${merge(
     var.tags,
