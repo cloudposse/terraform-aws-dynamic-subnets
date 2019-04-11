a9a7cd314db06c645b57a455a7bad00bba21fdb1
Igor Rodionov <goruha@gmail.com>
2019-04-12 01:34:44 +0600

    Update public.tf

--- public.tf
+++ public.tf
@@ -5,7 +5,7 @@ module "public_label" {
   name       = "${var.name}"
   delimiter  = "${var.delimiter}"
   attributes = "${compact(concat(var.attributes,list("public")))}"
-  tags       = "${merge(var.tags, map("type", "public"))}"
+  tags       = "${merge(var.tags, map("cpco.io/subnet/type", "public"))}"
 }
 
 module "public_subnet_label" {
@@ -14,7 +14,7 @@ module "public_subnet_label" {
   stage      = "${var.stage}"
   name       = "${var.name}"
   attributes = "${compact(concat(var.attributes,list("public")))}"
-  tags       = "${merge(var.tags, map("type", "public"))}"
+  tags       = "${merge(var.tags, map("cpco.io/subnet/type", "public"))}"
 }
 
 locals {
