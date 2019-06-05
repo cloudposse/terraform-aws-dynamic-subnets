1de418e9bd85d3ecb936141622d4977e48730587
aknysh <andriy.knysh@gmail.com>
2019-06-05 14:19:53 -0400

    Pin aws provider

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# Pin the `aws` provider
+# https://www.terraform.io/docs/configuration/providers.html
+# Any non-beta version >= 2.12.0 and < 2.13.0, e.g. 2.12.X
+provider "aws" {
+  version = "~> 2.12.0"
+}
+
+# Terraform
+#--------------------------------------------------------------
 terraform {
   required_version = "~> 0.11.0"
 }
