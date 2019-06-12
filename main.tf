0bc77ed91bc868c6aa5e9c78dcdda2826d2c96be
Sven Wagner-Boysen <sven.wagner-boysen@signavio.com>
2019-06-12 07:31:30 +0200

    removed provider configuration

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Pin the `aws` provider
 # https://www.terraform.io/docs/configuration/providers.html
 # Any non-beta version >= 2.12.0 and < 2.13.0, e.g. 2.12.X
-provider "aws" {
-  version = "~> 2.12.0"
-}
+# provider "aws" {
+#   version = "~> 2.12.0"
+# }
 
 # Terraform
 #--------------------------------------------------------------
