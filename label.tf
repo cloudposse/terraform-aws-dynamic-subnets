df80f388a360a4ddc9accbfdaa3c8bd55f142a04
Rohit Verma <rohit.verma@niki.ai>
2019-06-21 14:55:11 +0530

    upgraded-tf

--- label.tf
+++ label.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source              = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.13.0"
+  source              = "git::https://github.com/rverma-nikiai/terraform-null-label.git?ref=master"
   attributes          = var.attributes
   namespace           = var.namespace
   environment         = var.environment
