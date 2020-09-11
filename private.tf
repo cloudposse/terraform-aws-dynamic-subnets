497e59fbfe110be999bf21322470748642126125
Nuru <Nuru@users.noreply.github.com>
2020-09-10 23:05:52 -0700

    Update null-label to 0.19.2 everywhere

--- private.tf
+++ private.tf
@@ -1,5 +1,5 @@
 module "private_label" {
-  source = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.1"
+  source = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
 
   attributes = ["private"]
   tags = merge(
