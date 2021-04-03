a43663c5946b9f9de758232b322141f1d4fc3b75
Long Nguyen <tobernguyen@gmail.com>
2021-04-03 12:57:03 -0600

    chore(deps): remove unused provider

--- versions.tf
+++ versions.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 2.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.0"
-    }
     local = {
       source  = "hashicorp/local"
       version = ">= 1.2"
